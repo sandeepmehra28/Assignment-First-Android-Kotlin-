1dce6cb7aa220c2e9eee7c021aaf212b2d440bd3
sandeep mehra <sandeepmehra2259@gmail.com>
2025-09-25 23:58:23 +0530

    adding new files for assignment

--- app/src/main/java/com/example/assignment_app/DevelopmentActivity.kt
+++ app/src/main/java/com/example/assignment_app/DevelopmentActivity.kt
@@ -1,11 +1,11 @@
 package com.example.assignment_app
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.net.toUri
 
 class DevelopmentActivity : AppCompatActivity() {
@@ -13,8 +13,28 @@ class DevelopmentActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_development)
-
+        val androidDevCV = findViewById<CardView>(R.id.AndroidDevBtn)
+        val webDevCV = findViewById<CardView>(R.id.WebDevBtn)
+        val iosDevCV = findViewById<CardView>(R.id.IOSDevBtn)
+        val mlCV = findViewById<CardView>(R.id.MLBtn)
         val contactUsBtn = findViewById<TextView>(R.id.contactUsBtn)
+
+        androidDevCV.setOnClickListener {
+            val intent = Intent(this, AndroidDevActivity::class.java)
+            startActivity(intent)
+        }
+        webDevCV.setOnClickListener {
+            val intent = Intent(this, WebDevActivity::class.java)
+            startActivity(intent)
+        }
+        iosDevCV.setOnClickListener {
+            val intent = Intent(this, IOSDevActivity::class.java)
+            startActivity(intent)
+        }
+        mlCV.setOnClickListener {
+            val intent = Intent(this, MLActivity::class.java)
+            startActivity(intent)
+        }
         contactUsBtn.setOnClickListener {
             try{
                 val number = "9870550873"
---
+++ app/src/main/java/com/example/assignment_app/MLActivity.kt
@@ -0,0 +1,20 @@
+package com.example.assignment_app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MLActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_mlactivity)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
